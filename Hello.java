bf814a730256271d7f88705961c11c339ed25ec8
pg1411 <90827832+pg1411@users.noreply.github.com>
2021-10-26 21:54:26 +0530

    this is demo

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("printing numbers 1 to 10");
+        int i=1;
+        while(i<=10){
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+    }
+}
